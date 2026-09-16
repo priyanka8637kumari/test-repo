a166cea1e9960b5ac080e4da339f95bb6be51453
musishxo <musabamare@gmail.com>
2026-09-16 13:19:12 +0200

    rename txt name

--- miglani.py
+++ miglani.py
@@ -1,5 +1,5 @@
 # Create a new file and print a message
-file_name = "new_file.txt"
+file_name = "new_file2.txt"
 
 with open(file_name, "w", encoding="utf-8") as file:
     file.write("This is a newly created file.\n")
