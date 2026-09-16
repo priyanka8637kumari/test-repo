74ff94d136dd975f1c12757435ed3716f0080af0
Neeraj Miglani <neerajmiglani@outlook.com>
2026-09-16 10:51:18 +0200

    New file has been created

---
+++ miglani.py
@@ -0,0 +1,7 @@
+# Create a new file and print a message
+file_name = "new_file.txt"
+
+with open(file_name, "w", encoding="utf-8") as file:
+    file.write("This is a newly created file.\n")
+
+print(f"New file created: {file_name}")
